5dd88aee3546627f8d2cc2455a50936776f6b3c2
yerim <dev.yerimlee@gmail.com>
2026-04-06 17:17:44 +0900

    AI 채팅 프로젝트 파일 정리 및 업데이트

--- js/ui/common.js
+++ js/ui/common.js
@@ -1,48 +1,65 @@
-function appendAiMsg(html, delay){
-    return new Promise(resolve => {
-        setTimeout(()=>{
-            $('.chat-list').append(html);
+function appendAiMsg(html, delay) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            $(".chat-list").append(html);
             resolve();
         }, delay);
     });
 }
 
 // 채팅창 자동 스크롤 스크립트
-function scrollToLastMsg(){
+function scrollToLastMsg() {
     let lastMsg = $('.chat-list li[class*="-msg"]:last-child');
     console.log(lastMsg);
-    let inputHeight = $('.user-input-wrap').outerHeight(true);
-    let scrollTarget = lastMsg.position().top + $('.chat-wrap').scrollTop() - $('.chat-wrap').height() + lastMsg.outerHeight(true) + inputHeight + 16;
+    let inputHeight = $(".user-input-wrap").outerHeight(true);
+    let scrollTarget =
+        lastMsg.position().top +
+        $(".chat-wrap").scrollTop() -
+        $(".chat-wrap").height() +
+        lastMsg.outerHeight(true) +
+        inputHeight +
+        16;
 
-    $('.chat-wrap').animate({scrollTop: scrollTarget}, 300);
+    $(".chat-wrap").animate({ scrollTop: scrollTarget }, 300);
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     // what's scrolling
     // $('*').on('scroll', function(){
     //     console.log($(this).attr('class') || $(this).prop('tagName'));
     // });
 
     // 요소의 높낮이값 변수로 받아오는 스크립트
-    let inputHeight = $('.user-input-wrap').outerHeight(true);
-    document.documentElement.style.setProperty('--ai-chat-height', inputHeight + 'px');
-    let headerHeight = $('.header').outerHeight(true);
-    document.documentElement.style.setProperty('--header-height', headerHeight + 'px');
-    let footerHeight = $('.footer').outerHeight(true);
-    document.documentElement.style.setProperty('--footer-height', footerHeight + 'px');
+    let inputHeight = $(".user-input-wrap").outerHeight(true);
+    document.documentElement.style.setProperty(
+        "--ai-chat-height",
+        inputHeight + "px",
+    );
+    let headerHeight = $(".header").outerHeight(true);
+    document.documentElement.style.setProperty(
+        "--header-height",
+        headerHeight + "px",
+    );
+    let footerHeight = $(".footer").outerHeight(true);
+    document.documentElement.style.setProperty(
+        "--footer-height",
+        footerHeight + "px",
+    );
 
     // 채팅 구현 스크립트
-    $(document).on('click', '.ai-msg.has-choice-btn .option', function(){
+    $(document).on("click", ".ai-msg.has-choice-btn .option", function () {
         // 이미 선택된 radio option일 경우 return
-        if($(this).hasClass('is-selected')) return;
+        if ($(this).hasClass("is-selected")) return;
         // 이전 선택 초기화
-        $('.ai-msg.has-choice-btn .option').removeClass('is-selected');
-        $('.chat-list .appended, .chat-list .refresh-btn, .chat-list .prd-options').remove();
+        $(".ai-msg.has-choice-btn .option").removeClass("is-selected");
+        $(
+            ".chat-list .appended, .chat-list .refresh-btn, .chat-list .prd-options",
+        ).remove();
         // 현재 선택 처리
-        $(this).addClass('is-selected');
-        $(this).find('input[type="radio"]').prop('checked', true)
+        $(this).addClass("is-selected");
+        $(this).find('input[type="radio"]').prop("checked", true);
 
-        let optionTxt = $(this).find('.card-info span').text();
+        let optionTxt = $(this).find(".card-info span").text();
         let userMsg = `
             <li class="user-msg appended">
                 <div class="bubble"><span>${optionTxt}</span></div>
@@ -59,10 +76,10 @@ $(document).ready(function(){
                     <li class="prd-option">
                         <input type="radio" name="prd01">
                         <div class="prd-card">
-                            <img src="./../images/prd/ionic.png" alt="">
+                            <img src="./../images/prd/palisade_v2.png" alt="">
                             <div class="txt-wrap">
                                 <span class="txt1">준대형 SUV로 차박, 큰짐 운반에 최적</span>
-                                <span class="txt2">현대 페리세이드</span>
+                                <span class="txt2">현대 팰리세이드</span>
                                 <span class="txt3">5,000만원</span>
                             </div>
                         </div>
@@ -70,22 +87,22 @@ $(document).ready(function(){
                     <li class="prd-option">
                         <input type="radio" name="prd01">
                         <div class="prd-card">
-                            <img src="./../images/prd/ionic.png" alt="">
+                            <img src="./../images/prd/sorento.png" alt="">
                             <div class="txt-wrap">
-                                <span class="txt1">준대형 SUV로 차박, 큰짐 운반에 최적</span>
-                                <span class="txt2">기아</span>
-                                <span class="txt3">5,000만원</span>
+                                <span class="txt1">중형 SUV로 가족 캠핑에 적합</span>
+                                <span class="txt2">기아 쏘렌토</span>
+                                <span class="txt3">3,600만원</span>
                             </div>
                         </div>
                     </li>
                     <li class="prd-option">
                         <input type="radio" name="prd01">
                         <div class="prd-card">
-                            <img src="./../images/prd/ionic.png" alt="">
+                            <img src="./../images/prd/volvo.png" alt="">
                             <div class="txt-wrap">
-                                <span class="txt1">준대형 SUV로 차박, 큰짐 운반에 최적</span>
-                                <span class="txt2">현대 페리세이드</span>
-                                <span class="txt3">5,000만원</span>
+                                <span class="txt1">북유럽감성, 럭셔리 수입SUV</span>
+                                <span class="txt2">볼보 XC90</span>
+                                <span class="txt3">1억 1,000만원</span>
                             </div>
                         </div>
                     </li>
@@ -96,7 +113,7 @@ $(document).ready(function(){
             <li class="ai-msg appended">
                 <div class="bubble"><span>당신의 마음에 드는 차를 하나만 선택해보세요.</span></div>
             </li>
-        `
+        `;
         let refreshMsg = `
             <li class="ai-msg refresh-btn">
                 <button type="button">
@@ -106,26 +123,26 @@ $(document).ready(function(){
             </li>
         `;
 
-        $('.chat-list').append(userMsg);
+        $(".chat-list").append(userMsg);
         scrollToLastMsg();
         appendAiMsg(loadingMsg, 1000)
-            .then(()=>appendAiMsg(prdList, 1000))
-            .then(()=>appendAiMsg(choicemsg, 300))
-            .then(()=>appendAiMsg(refreshMsg, 300))
-            .then(()=>scrollToLastMsg());
+            .then(() => appendAiMsg(prdList, 1000))
+            .then(() => appendAiMsg(choicemsg, 300))
+            .then(() => appendAiMsg(refreshMsg, 300))
+            .then(() => scrollToLastMsg());
     });
-    $(document).on('click', '.ai-msg.has-prd-btn .prd-option', function(){
-        if($(this).hasClass('is-selected')) return;
+    $(document).on("click", ".ai-msg.has-prd-btn .prd-option", function () {
+        if ($(this).hasClass("is-selected")) return;
 
-        $('.ai-msg.has-prd-btn .prd-option').removeClass('is-selected');
+        $(".ai-msg.has-prd-btn .prd-option").removeClass("is-selected");
         // $('.chat-list .reminder, .chat-list .refresh-btn, .chat-list .prd-options').remove();
-        $('.ai-msg.has-prd-btn').nextAll().remove();
+        $(".ai-msg.has-prd-btn").nextAll().remove();
 
         // 현재 선택 처리
-        $(this).addClass('is-selected');
-        $(this).find('input[type="radio"]').prop('checked', true)
+        $(this).addClass("is-selected");
+        $(this).find('input[type="radio"]').prop("checked", true);
 
-        let prdTxt = $(this).find('.txt-wrap .txt2').text();
+        let prdTxt = $(this).find(".txt-wrap .txt2").text();
         let userMsg = `
             <li class="user-msg appended">
                 <div class="bubble"><span>${prdTxt}</span></div>
@@ -144,69 +161,76 @@ $(document).ready(function(){
                 <ul class="info-list">
                     <li class="info-btn">
                         <div class="rd-wrap">
-                            <input type="radio" name="prd1">
-                            <span>차량 견적보기</span>
+                            <a href="./result01.html">
+                                <span>차량 견적보기</span>
+                            </a>
                         </div>
                     </li>
                     <li class="info-btn">
                         <div class="rd-wrap">
-                            <input type="radio" name="prd1">
-                            <span>신차 할부</span>
+                            <a href="#">
+                                <span>신차 할부</span>
+                            </a>
                         </div>
                     </li>
                     <li class="info-btn">
                         <div class="rd-wrap">
-                            <input type="radio" name="prd1">
-                            <span>신차 리스</span>
+                            <a href="#">
+                                <span>신차 리스</span>
+                            </a>
                         </div>
                     </li>
                     <li class="info-btn">
                         <div class="rd-wrap">
-                            <input type="radio" name="prd1">
-                            <span>장기렌터카</span>
+                            <a href="#">
+                                <span>장기렌터가</span>
+                            </a>
                         </div>
                     </li>
                 </ul>
             </li>
-        `
+        `;
 
-        $('.chat-list').append(userMsg);
+        $(".chat-list").append(userMsg);
         scrollToLastMsg();
         appendAiMsg(gotPrdName, 300)
-            .then(()=>appendAiMsg(infoTxt, 500))
-            .then(()=>scrollToLastMsg());
+            .then(() => appendAiMsg(infoTxt, 500))
+            .then(() => scrollToLastMsg());
 
         // 가로 스크롤
-        let $prdList = $(this).closest('.prd-list');
+        let $prdList = $(this).closest(".prd-list");
         let itemLeft = $(this).position().left;
         let currentScroll = $prdList.scrollLeft();
 
-        $prdList.animate({
-            scrollLeft: currentScroll + itemLeft - 32
-        }, 300);
+        $prdList.animate(
+            {
+                scrollLeft: currentScroll + itemLeft - 32,
+            },
+            300,
+        );
     });
-    $(document).on('click', '.ai-msg.prd-options .info-btn', function(){
-         // 이미 선택된 radio option일 경우 return
-        if($(this).hasClass('is-selected')) return;
+    $(document).on("click", ".ai-msg.prd-options .info-btn", function () {
+        // 이미 선택된 radio option일 경우 return
+        if ($(this).hasClass("is-selected")) return;
 
-        let $currentPrdOptions = $(this).closest('.ai-msg.prd-options');
+        let $currentPrdOptions = $(this).closest(".ai-msg.prd-options");
 
         // 이전 선택 초기화
-        $('.ai-msg.prd-options .info-btn').removeClass('is-selected');
+        $(".ai-msg.prd-options .info-btn").removeClass("is-selected");
         // $('.chat-list .appended, .ai-msg.prd-options').remove();
         $currentPrdOptions.nextAll().remove();
         // 현재 선택 처리
-        $(this).addClass('is-selected');
-        $(this).find('input[type="radio"]').prop('checked', true)
+        $(this).addClass("is-selected");
+        $(this).find('input[type="radio"]').prop("checked", true);
 
-        let choicedTxt = $(this).find('.rd-wrap span').text();
+        let choicedTxt = $(this).find(".rd-wrap span").text();
         let userMsg = `
             <li class="user-msg appended">
                 <div class="bubble">
                     <span>${choicedTxt}</span>
                 </div>
             </li>
-        `
+        `;
 
         let nextAiMsg = `
             <li class="ai-msg">
@@ -214,7 +238,7 @@ $(document).ready(function(){
                     <span>자동차 대출에 대해서 궁금하신가요? 쉽게 알려드릴게요.</span>
                 </div>
             </li>
-        `
+        `;
         let loanInfoMsg = `
             <li class="ai-msg prd-options">
                 <p><i class="icon-money-pocket"></i>어떤 대출이 필요하신가요?</p>
@@ -239,23 +263,111 @@ $(document).ready(function(){
                     </li>
                 </ul>
             </li>
-        `
+        `;
 
-        $('.chat-list').append(userMsg);
+        $(".chat-list").append(userMsg);
         scrollToLastMsg();
         appendAiMsg(nextAiMsg, 1000)
-            .then(()=>appendAiMsg(loanInfoMsg, 300))
-            .then(()=>scrollToLastMsg());
+            .then(() => appendAiMsg(loanInfoMsg, 300))
+            .then(() => scrollToLastMsg());
     });
 
     // 화면 이동 시 채팅 시작 애니메이션
-    let items = $('.chat-list > li');
+    let items = $(".chat-list > li");
 
-    items.each(function(index){
+    items.each(function (index) {
         let $item = $(this);
 
-        setTimeout(function(){
-            $item.addClass('visible');
+        setTimeout(function () {
+            $item.addClass("visible");
         }, index * 1000);
+    });
+
+    // json 이미지 로딩
+    if ($("#animatesStar").length) {
+        lottie.loadAnimation({
+            container: document.getElementById("animatesStar"),
+            renderer: "svg",
+            loop: true,
+            autoplay: true,
+            animationData: aiStartJson,
+        });
+    }
+
+    // 차량 캐로셀
+    let $ul = $(".prd-list-wrap ul");
+    let $items = $(".prd-list-wrap ul li");
+    let totalItems = $items.length;
+    let currentIndex = 1;
+
+    function updateCarousel() {
+        let ulWidth = $ul.width();
+        let itemWidth = $items.eq(0).outerWidth(true);
+        let centerOffset = ulWidth / 2 - itemWidth / 2;
+
+        $items.each(function (index) {
+            let diff = index - currentIndex;
+            let translateX = centerOffset + diff * (itemWidth + 8);
+
+            $(this).css({
+                transform: `translateX(${translateX}px)`,
+                opacity: index === currentIndex ? 1 : 0.5,
+                cursor: index === currentIndex ? "default" : "pointer",
+                position: "absolute",
+            });
+            $(this)
+                .find(".desc-wrap")
+                .css({
+                    opacity: index === currentIndex ? 1 : 0,
+                    transition: "opacity 0.4s ease",
+                });
+        });
+    }
+
+    let maxHeight = 0;
+    $items.each(function () {
+        if ($(this).outerHeight(true) > maxHeight) {
+            console.log($(this));
+            console.log($(this).outerHeight());
+            maxHeight = $(this).outerHeight(true);
+        }
+    });
+    $ul.css({ position: "relative", height: maxHeight + 40 + "px" });
+    updateCarousel();
+
+    // 선택한 차량의 모델정보 가져오기
+    let prdData = {
+        prdPalisade: {
+            desc01: "현대 팰리세이드",
+            desc02: "45,000,000원",
+            desc03: "합리적인 가격과 높은 안전성의<br>팰리세이드와 함께하세요.",
+        },
+        prdTesla: {
+            desc01: "테슬라 모델 3",
+            desc02: "50,000,000원",
+            desc03: "글로벌 인기 모델<br>테슬라 모델 3와 함께하세요.",
+        },
+        prdKia: {
+            desc01: "기아 EV6",
+            desc02: "47,000,000원",
+            desc03: "세련된 디자인과 안정적인<br>기아 EV6와 함께하세요.",
+        },
+    };
+
+    function updateDesc(id) {
+        let data = prdData[id];
+        if (!data) return;
+        $(".desc-txt-wrap .desc-wrap .desc01").html(data.desc01);
+        $(".desc-txt-wrap .desc-wrap .desc02").html(data.desc02);
+        $(".desc-txt-wrap .desc-wrap .desc03").html(data.desc03);
+    }
+    updateDesc($items.eq(currentIndex).attr("id"));
+
+    $(document).on("click", ".prd-list-wrap ul li", function () {
+        const clickedIndex = $items.index(this);
+        if (clickedIndex === currentIndex) return;
+        currentIndex = clickedIndex;
+        updateCarousel();
+        updateDesc($(this).attr("id"));
     });
 });
